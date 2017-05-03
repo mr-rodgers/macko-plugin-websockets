f150377e4481c3f9692a8dc4d494dd1ea5e26668
Te-jé Rodgers <tjd.rodgers@gmail.com>
2017-05-03 14:36:13 -0400

    update compiled js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Foo {
+const Websocket = require("ws");
+class WebsocketHandler {
+    constructor() {
+        this.protocol = "ws://";
+        this.name = "Websockets";
+        this.supportedPayloads = ["text/plain"];
+    }
+    watch(url) {
+        let ws = new Websocket(url);
+        return new WebsocketWatcher(ws);
+    }
 }
-exports.default = Foo;
+exports.default = WebsocketHandler;
+class WebsocketWatcher extends NodeJS.EventEmitter {
+    constructor(ws) {
+        super();
+        this.ws = ws;
+        // Catch and emit message events
+        ws.on("message", (data, flags) => {
+            this.emit("message", data, flags);
+        });
+        // Catch and emit error events
+        ws.on("error", err => this.emit("error", err));
+        // Catch and emit close events
+        ws.on("close", (code, message) => this.emit("close", code, message));
+    }
+    close() {
+        this.ws.close();
+    }
+}
+//# sourceMappingURL=index.js.map
